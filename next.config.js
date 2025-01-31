718d3ea1bf1ad0ea529ef8aee6dabfac87344ab3
georgerokvich <georgerokvich@gmail.com>
2025-01-31 16:46:50 +0100

    Add picsum.photos to Next.js image domains

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'source.unsplash.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos'
       }
     ]
   },
